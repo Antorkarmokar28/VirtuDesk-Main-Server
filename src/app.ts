bfd0efdb7ed4543f67779c59c2bc397564d242a5
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-06-25 01:05:17 +0600

    This project in server setup done

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app: Application = express();
+// using parser
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
+// connect with application route
+// app.use("/api");
+const testServer = async (req: Request, res: Response) => {
+  res.send({
+    status: true,
+    message: "VirtuDesk Server is running",
+  });
+};
+app.get("/", testServer);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+// getting-started.js
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+import { IncomingMessage, Server, ServerResponse } from "http";
+let server: Server<typeof IncomingMessage, typeof ServerResponse> | undefined;
+//main server
+async function mainServer() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+// call the main server
+mainServer();
+// using unhandledRejection for asynchonouse code
+process.on("unhandledRejection", () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+// using uncaughtException for synchonouse code
+process.on("uncaughtException", () => {
+  process.exit(1);
+});
