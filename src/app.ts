d4253c444a2877216b8544339f903021cc375950
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-06-25 11:59:52 +0600

    This project in error handling done

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import router from "./app/routes";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import router from './app/routes';
+import globalErrorHandeling from './app/middlewares/globalErrorHandeling';
 const app: Application = express();
 // using parser
 app.use(express.json());
-app.use(cors({ origin: "", credentials: true }));
+app.use(cors({ origin: '', credentials: true }));
 app.use(cookieParser());
 // connect with application router
-app.use("/api", router);
+app.use('/api', router);
 const testServer = async (req: Request, res: Response) => {
   res.send({
     status: true,
-    message: "VirtuDesk Server is running",
+    message: 'VirtuDesk Server is running',
   });
 };
-app.get("/", testServer);
-
+app.get('/', testServer);
+// Global error handling
+app.use(globalErrorHandeling);
 export default app;
